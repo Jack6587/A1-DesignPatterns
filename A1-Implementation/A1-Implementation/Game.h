1ad568feb304675d017c2020e340e454ba771feb
Jack6587 <klejt002@mymail.unisa.edu.au>
2025-04-07 23:47:29 +0930

    Added shell to the game class
    Need to add shell code to other classes, which has already prompted me to fix up my class diagram (like forgetting to add return types etc)

--- A1-Implementation/A1-Implementation/Game.h
+++ A1-Implementation/A1-Implementation/Game.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <vector>
+#include "Card.h"
+#include "Player.h"
+
+typedef std::vector<Card*> CardCollection;
+
+class Game {
+public:
+	Game(); //constructor
+	~Game(); //destructor
+
+	void startGame();
+	void endGame();
+
+	void createDeck();
+	void shuffleDeck(CardCollection cards);
+	void initialisePlayers();
+
+	void drawCard();
+	void promptPlayerToDraw();
+	void outputScores();
+	void switchPlayer();
+	void gameOver();
+};
