5d0042c5e500dee35beb02e0ec2fe8dcb4f60471
Jack6587 <klejt002@mymail.unisa.edu.au>
2025-04-07 23:23:25 +0930

    Initial code commit

---
+++ A1-Implementation/A1-Implementation/A1-Implementation.cpp
@@ -0,0 +1,12 @@
+﻿// A1-Implementation.cpp : Defines the entry point for the application.
+//
+
+#include "A1-Implementation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ A1-Implementation/A1-Implementation/A1-Implementation.h
@@ -0,0 +1,8 @@
+﻿// A1-Implementation.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
