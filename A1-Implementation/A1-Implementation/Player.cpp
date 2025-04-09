e141d82e7b316e127fed43836fd92d07bdb05973
Jack6587 <klejt002@mymail.unisa.edu.au>
2025-04-09 18:46:48 +0930

    Brief changes
    Added a Player in .cpp constructor, changed some comments etc in Player.h

---
+++ A1-Implementation/A1-Implementation/Player.cpp
@@ -0,0 +1,8 @@
+#include "Player.h"
+
+Player::Player() {
+	std::string names[] = { "Sam", "Billy", "Jen", "Bob", "Sally",
+	"Joe", "Sue", "Sasha", "Tina", "Marge" };
+	_name = names[rand() % 10]; // sets a random name in the constructor
+	_totalScore = 0;
+}
--- A1-Implementation/A1-Implementation/Player.h
+++ A1-Implementation/A1-Implementation/Player.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include "Card.h"
+#include "Game.h"
 
 class Player {
 public:
@@ -10,15 +11,15 @@ public:
 
 	void pickUpCard();
 	void moveToBank(Card* card); // card is a pointer - because it's always changing (unlike a reference)
-	void playCard(Card* card, CardCollection* deck); // ? moves card to play area
-
+	void playCard(Card* card, CardCollection* deck); // moves card to play area, removes it from the current provided deck
+	
 	void isBust();
 	void accessBank();
 	void displayBank();
 
 private:
-	std::string name;
-	int totalScore;
-	CardCollection playArea;
-	CardCollection bank;
+	std::string _name;
+	int _totalScore;
+	CardCollection _playArea;
+	CardCollection _bank;
 };
