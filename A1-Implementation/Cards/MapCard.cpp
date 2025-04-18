6eaffc1ad27fe3eec84c447e66032a0546daa84a
Jack6587 <klejt002@mymail.unisa.edu.au>
2025-04-18 12:22:49 +0930

    Mostly implemented the map card play method
    Basically just getting three cards as per instructions and then getting user input to determine which one to use. One thing I was unsure about was whether the other cards drawn get put into the play area or go back to the discard pile - but I think they go back to the discard pile

--- A1-Implementation/Cards/MapCard.cpp
+++ A1-Implementation/Cards/MapCard.cpp
@@ -14,11 +14,31 @@ void MapCard::play(Game& game, Player& player) {
 	CardCollection& discardCards = game.getDiscardPile().getCards();
 	CardCollection drawnCards;
 
+	if (discardCards.empty()) {
+		std::cout << "No cards in the discard pile!\n";
+		return;
+	}
+
 	for (int i = 0; i < 3; i++) {
 		Card* card = discardCards.back();
 		discardCards.pop_back();
 		drawnCards.push_back(card);
 		player.getPlayArea().addCard(card);
+		std::cout << card->str() << "\n";
+	}
+
+	int choice = 0;
+	while (choice > drawnCards.size() || choice < 1) {
+		std::cout << "Choose a card to play (1-3): ";
+		std::cin >> choice;
+	}
+
+	Card* chosenCard = drawnCards[choice-1];
+	player.playCard(chosenCard, game);
+	drawnCards.erase(chosenCard);
+
+	for (Card* card : drawnCards){
+		game.getDiscardPile().addCard(card);
 	}
 }
 
